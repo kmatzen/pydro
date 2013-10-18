2242591aa2ca7a90dcc584d0d520ba88fc3b9f87
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-10-18 16:36:57 -0400

    Added loss adjustment hook for filters.  Simplified parse interface by packaging up pyramid inside filtered model.

--- src/pydro/train.py
+++ src/pydro/train.py
@@ -2,7 +2,7 @@ from pydro.core import TreeNode
 
 import numpy
 
-def BuildFeatureVector (model, detection, pyra):
-    features = detection.child.symbol.GetFeatures (model, detection.child, pyra)
+def BuildFeatureVector (model, detection):
+    features = detection.child.symbol.GetFeatures (model, detection.child)
     return features
 
--- tests/train_tests.py
+++ tests/train_tests.py
@@ -1,6 +1,7 @@
 from pydro.io import *
 from pydro.train import *
 from pydro.features import *
+from pydro.core import *
 
 import itertools
 
@@ -15,6 +16,20 @@ def train_test():
     filtered_model = model.Filter(pyramid)
 
     detections = [d for i,d in itertools.izip(xrange(1), filtered_model.Parse(-1))]
+    assert detections[0].loss is None
 
-    feature_vector = [BuildFeatureVector(model, d, pyramid) for d in detections]
+    feature_vector = [BuildFeatureVector(filtered_model, d) for d in detections]
 
+def loss_adjustment_test():
+    model = LoadModel('tests/example.dpm')
+
+    image = scipy.misc.imread('tests/000034.jpg')
+    pyramid = BuildPyramid(image, model=model)
+
+    def loss_adjustment (score, block):
+        return [Score(scale=s.scale, score=s.score+1) for s in score]
+
+    filtered_model = model.Filter(pyramid, loss_adjustment=loss_adjustment)
+
+    detections = [d for i,d in itertools.izip(xrange(1), filtered_model.Parse(-1))]
+    assert math.fabs(detections[0].loss - 9) < 1e-5
