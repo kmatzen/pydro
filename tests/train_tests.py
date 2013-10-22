6be986f69ba1f7177b9a53e1499ba0608271548a
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-10-21 22:10:19 -0400

    Added rough version of optimization routine.

--- tests/train_tests.py
+++ tests/train_tests.py
@@ -18,7 +18,7 @@ def train_test():
     detections = [d for i,d in itertools.izip(xrange(1), filtered_model.Parse(-1))]
     assert detections[0].loss is None
 
-    feature_vector = [BuildFeatureVector(d) for d in detections]
+    feature_vector = [BuildFeatureVector(d, belief=True, positive=True) for d in detections]
 
 def loss_adjustment_test():
     model = LoadModel('tests/example.dpm')
@@ -43,7 +43,19 @@ def neg_latent_test():
     pyramid = BuildPyramid (image, model=model, interval=4)
 
     M = 1
-    features = NegativeLatentFeatures (model, pyramid, M)
+    example = NegativeLatentFeatures (model, pyramid, M)
+
+    assert len(example) == 2
+
+    for entry in example:
+        new_score = ScoreVector(entry)
+        if entry.detection is not None:
+            print(entry.detection.s, entry.loss)
+            assert math.fabs(entry.detection.s - new_score) < 1e-4
+        else:
+            print(0, entry.loss)
+            assert math.fabs(new_score) < 1e-4
+
 
 def pos_latent_test():
     model = LoadModel('tests/example.dpm')
@@ -58,7 +70,19 @@ def pos_latent_test():
         return score
 
     M = 1
-    pos, neg = PositiveLatentFeatures (model, pyramid, belief_adjustment, loss_adjustment, M)
+    example = PositiveLatentFeatures (model, pyramid, belief_adjustment, loss_adjustment, M)
+
+    assert len(example) == 3
+
+    for entry in example:
+        new_score = ScoreVector(entry)
+        if entry.detection is not None:
+            print(entry.detection.s, entry.loss)
+            assert math.fabs(entry.detection.s - new_score) < 1e-4
+        else:
+            print(0, entry.loss)
+            assert math.fabs(new_score) < 1e-4
+
 
 def overlap_loss_test():
     interval_fg = 5
@@ -78,7 +102,19 @@ def overlap_loss_test():
     belief_adjustment = OverlapLossAdjustment(model, pyramid, 0.7, -numpy.inf, model.start.rules, bbox)
 
     M = 1
-    pos, neg = PositiveLatentFeatures (model, pyramid, belief_adjustment, loss_adjustment, M)
+    example = PositiveLatentFeatures (model, pyramid, belief_adjustment, loss_adjustment, M)
+
+    assert len(example) == 3
+
+    for entry in example:
+        new_score = ScoreVector(entry)
+        if entry.detection is not None:
+            print(entry.detection.s, entry.loss)
+            assert math.fabs(entry.detection.s - new_score) < 1e-4
+        else:
+            print(0, entry.loss)
+            assert math.fabs(new_score) < 1e-4
+
 
 def optimize_test():
     interval_fg = 5
@@ -92,14 +128,22 @@ def optimize_test():
 
     bbox = BBox(x1=detection.x1, y1=detection.y1, x2=detection.x2, y2=detection.y2)
 
-    pyramid = BuildPyramid (image, model=model, interval=interval_fg)
+    pyramid = BuildPyramid (image, model=model)#, interval=interval_fg)
 
     loss_adjustment = OverlapLossAdjustment(model, pyramid, 0.5, 1, model.start.rules, bbox)
     belief_adjustment = OverlapLossAdjustment(model, pyramid, 0.7, -numpy.inf, model.start.rules, bbox)
 
     M = 1
-    pos, neg = PositiveLatentFeatures (model, pyramid, belief_adjustment, loss_adjustment, M)
+    example = PositiveLatentFeatures (model, pyramid, belief_adjustment, loss_adjustment, M)
+    assert len(example) == 3
 
-    examples = pos + neg
+    for entry in example:
+        new_score = ScoreVector(entry)
+        if entry.detection is not None:
+            print(entry.detection.s, entry.loss)
+            assert math.fabs(entry.detection.s - new_score) < 1e-4
+        else:
+            print(0, entry.loss)
+            assert math.fabs(new_score) < 1e-4
 
-    Optimize (model, examples)
+    Optimize (model, [example])
